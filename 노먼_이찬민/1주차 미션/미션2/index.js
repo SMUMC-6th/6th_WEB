f2da1ec34a001a1865251179bbf3d8f54645332a
chanmin Lee <dlcksals87@gmail.com>
2024-03-23 22:31:49 +0900

    💄 CHORE : 스타일 지정, 삭제 기능 추가

--- 노먼_이찬민/1주차 미션/미션2/index.js
+++ 노먼_이찬민/1주차 미션/미션2/index.js
@@ -8,7 +8,36 @@ todoForm.addEventListener("submit", function (event) {
 
   createTodoListItem(todoInput.value);
   todoInput.value = "";
-});
+}); 
+
+// remove 버튼 이벤트 콜백함수
+function removeCallBack(event) {
+  const removeItem = event.target.parentElement;
+  removeItem.remove();
+}
+
+// complete 버튼 이벤트 콜백함수 ... 어렵당
+function completeCallBack(event) {
+  // 미리 이벤트 타겟(완료버튼)의 이벤트리스너 삭제 후 완료리스트로 전달하기 -> 그러면 완료 버튼이 안눌리지;; 
+  // 이벤트 발생 후 아이템 삭제까지는 버튼이 작동중이어야함 -> 새로 버튼 만들고 바꿔버리자.
+  // 새로운 삭제 버튼 생성
+  const removeButton = document.createElement("button");
+  removeButton.innerText = "삭제";
+  removeButton.addEventListener("click", removeCallBack);
+
+  // 이벤트 타겟(완료 버튼)의 부모 요소(할 일 리스트 아이템) 변수에 저장
+  const todoItem = event.target.parentElement;
+
+  // !!! 기존의 버튼 제거 - todoItem이라는 객체변수에서 쿼리셀렉터(type)으로 찾아서 제거
+  const completeButton = todoItem.querySelector("button");
+  todoItem.removeChild(completeButton);
+
+  // 완료 리스트로 항목 이동
+  completeList.appendChild(todoItem);
+
+  // 새로운 삭제 버튼 추가
+  todoItem.appendChild(removeButton);
+}
 
 function createTodoListItem(str) {
   const li = document.createElement("li");
@@ -22,11 +51,7 @@ function createTodoListItem(str) {
   button.innerText = "완료";
 
   // 버튼에 이벤트 달기 - 이벤트 타겟(버튼)의 상위요소(리스트 아이템)를 삭제 및 이동
-  button.addEventListener("click", function (event) {
-    const del = event.target.parentElement;
-    del.remove();
-    completeList.appendChild(del);
-  })
+  button.addEventListener("click", completeCallBack);
   todoList.appendChild(li);
   console.log(li);
 }
