ade39922a93b5d65101e761284db9046cb90cf22
chanmin Lee <dlcksals87@gmail.com>
2024-03-26 17:18:20 +0900

    ⚡️ 미션 1 REFACTOR : 요소 선언 최소화 및 함수형 기능 정의

--- 노먼_이찬민/1주차 미션/미션1/index.js
+++ 노먼_이찬민/1주차 미션/미션1/index.js
@@ -5,183 +5,128 @@
 // 그리고 dom 요소 찾아오는 구문 오류 발생 시, 이벤트리스너가 안되는 경우 있음.
 // 만약 문제 발생하면 변수 할당 문제인지 먼저 검토
 
+// 피드백 1 : DOM 객체 너무 많음. 메세지는 innerText로 변경
+// 피드백 2 : 문자열 trim 해주기
+
+
 let membershipForm = document.getElementById("membership");
 
-let nameCheck = 0;
-let emailCheck = 0;
-let ageCheck = 0;
-let passwordCheck = 0;
-let passwordValidCheck = 0;
+let nameMessage = document.querySelector(".nameMessage");
+let emailMessage = document.querySelector(".emailMessage");
+let ageMessage = document.querySelector(".ageMessage");
+let passwordMessage = document.querySelector(".passwordMessage");
+let passwordValidMessage = document.querySelector(".passwordValidMessage");
 
-let nameInputElement = document.getElementById("userName");
-let nameSuccessElement = document.querySelector("#name .successMessage");
-let nameFailElement = document.querySelector("#name .failMessage");
+const emailPattern = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-za-z0-9\-]+/;
+const passwordPattern = /^(?=.*[a-zA-Z])(?=.*[!@#$%^&*+=-])(?=.*[0-9]).{4,12}$/;
 
-let emailInputElement = document.getElementById("userEmail");
-let emailSuccessElement = document.querySelector("#email .successMessage");
-let emailFailElement = document.querySelector("#email .failMessage");
-let emailPattern = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-za-z0-9\-]+/;
-
-let ageInputElement = document.getElementById("userAge");
-let ageSuccessElement = document.querySelector("#age .successMessage");
-let ageNumFailElement = document.querySelector("#age .numFailMessage");
-let ageMinusFailElement = document.querySelector("#age .minusFailMessage");
-let ageFractionFailElement = document.querySelector(
-  "#age .fractionFailMessage"
-);
-let ageMinFailElement = document.querySelector("#age .minFailMessage");
-
-let passwordInputElement = document.getElementById("userPassword");
-let passwordSuccessElement = document.querySelector(
-  "#password .successMessage"
-);
-let passwordMinFailElement = document.querySelector(
-  "#password .minFailMessage"
-);
-let passwordMaxFailElement = document.querySelector(
-  "#password .maxFailMessage"
-);
-let passwordContentFailElement = document.querySelector(
-  "#password .contentFailMessage"
-);
-
-let passwordValidInputElement = document.getElementById("userPasswordValid");
-let passwordValidSuccessElement = document.querySelector(
-  "#passwordValid .successMessage"
-);
-let passwordValidFailElement = document.querySelector(
-  "#passwordValid .failMessage"
-);
-
-function validEmail(str) {
-  // T/F 반환 - match는 [ 문자열.match(확인패턴) ] 형태로 사용
-  // tf 반환이 아니었네... 해당 문자열 반환하느라 안됐구나...
-  return emailPattern.test(str);
+function nameCheck(name) {
+  if (name.length === 0) {
+    nameMessage.innerText = "필수 입력 항목입니다.";
+    nameMessage.style.color = "red";
+    return 0;
+  }
+  else{
+    nameMessage.innerText = "멋진 이름이네요!"
+    nameMessage.style.color = "green";
+    return 1;
+  }
 }
 
-function validPassword(str) {
-  // T/F 반환 - test는 [ 확인패턴.test(문자열) ] 형태로 사용
-  // 정규표현식의 마지막 {최소, 최대} 안채우면 안되는듯?
-  return /^(?=.*[a-zA-Z])(?=.*[!@#$%^&*+=-])(?=.*[0-9]).{4,12}$/.test(str);
+function emailCheck(email) {
+  if (emailPattern.test(email) === false) {
+    emailMessage.innerText = "올바른 이메일 형식이 아닙니다.";
+    emailMessage.style.color = "red";
+    return 0;
+  }
+
+  else{
+    emailMessage.innerText = "올바른 이메일 형식입니다!";
+    emailMessage.style.color = "green";
+    return 1;
+  }
 }
 
-function isMatch(password1, password2) {
-  return password1 === password2;
+function ageCheck(age) {
+  if (Number.isNaN(age) === true) {
+    ageMessage.innerText = "나이는 숫자 형식이어야 합니다.";
+    ageMessage.style.color = "red";
+    return 0;
+  }
+  else if (age < 0) {
+    ageMessage.innerText = "나이는 음수가 될 수 없습니다.";
+    ageMessage.style.color = "red";
+    return 0;
+  }
+
+  else if (age.includes(".") === true) {
+    ageMessage.innerText = "나이는 소수가 될 수 없습니다.";
+    ageMessage.style.color = "red";
+    return 0;
+  }
+
+  else if (age < 19) {
+    ageMessage.innerText = "미성년자는 가입할 수 없습니다.";
+    ageMessage.style.color = "red";
+    return 0;
+  }
+  
+  else{
+    ageMessage.innerText = "올바른 나이 형식입니다!";
+    ageMessage.style.color = "green";
+    return 1;
+  }
+}
+
+function passwordCheck(password) {
+  if (password.length < 4) {
+    passwordMessage.innerText = "비밀번호는 4자리 이상입니다.";
+    passwordMessage.style.color = "red";
+    return 0;
+  }
+  else if (password.length > 12) {
+    passwordMessage.innerText = "비밀번호는 12자리 이하입니다.";
+    passwordMessage.style.color = "red";
+    return 0;
+  }
+  else if (passwordPattern.test(password) === false) {
+    passwordMessage.innerText = "비밀번호는 영어,숫자,특수문자가 모두 포함되어야 합니다.";
+    passwordMessage.style.color = "red";
+    return 0;
+  }
+  else{
+    passwordMessage.innerText = "올바른 비밀번호 형식입니다!";
+    passwordMessage.style.color = "green";
+    return 1;
+  }
+}
+
+function passwordValidCheck(password, passwordValid) {
+  if ((passwordValid === password) === false || passwordValid.length === 0) {
+    passwordValidMessage.innerText = "비밀번호가 일치하지 않습니다.";
+    passwordValidMessage.style.color = "red";
+    return 0;
+  }
+  else{
+    passwordValidMessage.innerText = "비밀번호가 일치합니다!";
+    passwordValidMessage.style.color = "green";
+    return 1;
+  }
 }
 
 membershipForm.addEventListener("submit", function (event) {
-  event.preventDefault();
+  event.preventDefault(); 
 
-  //이름
-  if (nameInputElement.value.length === 0) {
-    nameSuccessElement.classList.add("hide");
-    nameFailElement.classList.remove("hide");
-    nameCheck = 0;
-  } else {
-    nameSuccessElement.classList.remove("hide");
-    nameFailElement.classList.add("hide");
-    nameCheck = 1;
-  }
-
-  //이메일
-  if (validEmail(emailInputElement.value) === true) {
-    emailSuccessElement.classList.remove("hide");
-    emailFailElement.classList.add("hide");
-    emailCheck = 1;
-  } else {
-    emailSuccessElement.classList.add("hide");
-    emailFailElement.classList.remove("hide");
-    emailCheck = 0;
-  }
-
-  // 나이
-  if (
-    isNaN(ageInputElement.value) === true ||
-    ageInputElement.value.length === 0
-  ) {
-    ageSuccessElement.classList.add("hide");
-    ageNumFailElement.classList.remove("hide");
-    ageFractionFailElement.classList.add("hide");
-    ageMinusFailElement.classList.add("hide");
-    ageMinFailElement.classList.add("hide");
-    ageCheck = 0;
-  } else if (parseInt(ageInputElement.value) < 0) {
-    ageSuccessElement.classList.add("hide");
-    ageNumFailElement.classList.add("hide");
-    ageFractionFailElement.classList.add("hide");
-    ageMinusFailElement.classList.remove("hide");
-    ageMinFailElement.classList.add("hide");
-    ageCheck = 0;
-  } else if (parseInt(ageInputElement.value) < 19) {
-    ageSuccessElement.classList.add("hide");
-    ageNumFailElement.classList.add("hide");
-    ageFractionFailElement.classList.add("hide");
-    ageMinusFailElement.classList.add("hide");
-    ageMinFailElement.classList.remove("hide");
-    ageCheck = 0;
-  } else if (Number.isInteger(+ageInputElement.value) === false) {
-    ageSuccessElement.classList.add("hide");
-    ageNumFailElement.classList.add("hide");
-    ageFractionFailElement.classList.remove("hide");
-    ageMinusFailElement.classList.add("hide");
-    ageMinFailElement.classList.add("hide");
-    ageCheck = 0;
-  } else {
-    ageSuccessElement.classList.remove("hide");
-    ageNumFailElement.classList.add("hide");
-    ageFractionFailElement.classList.add("hide");
-    ageMinusFailElement.classList.add("hide");
-    ageMinFailElement.classList.add("hide");
-    ageCheck = 1;
-  }
-
-  // 비밀번호
-  if (passwordInputElement.value.length < 4) {
-    passwordSuccessElement.classList.add("hide");
-    passwordMinFailElement.classList.remove("hide");
-    passwordMaxFailElement.classList.add("hide");
-    passwordContentFailElement.classList.add("hide");
-    passwordCheck = 0;
-  } else if (passwordInputElement.value.length > 12) {
-    passwordSuccessElement.classList.add("hide");
-    passwordMinFailElement.classList.add("hide");
-    passwordMaxFailElement.classList.remove("hide");
-    passwordContentFailElement.classList.add("hide");
-    passwordCheck = 0;
-  } else if (validPassword(passwordInputElement.value) === false) {
-    passwordSuccessElement.classList.add("hide");
-    passwordMinFailElement.classList.add("hide");
-    passwordMaxFailElement.classList.add("hide");
-    passwordContentFailElement.classList.remove("hide");
-    passwordCheck = 0;
-  } else {
-    passwordSuccessElement.classList.remove("hide");
-    passwordMinFailElement.classList.add("hide");
-    passwordMaxFailElement.classList.add("hide");
-    passwordContentFailElement.classList.add("hide");
-    passwordCheck = 1;
-  }
-
-  // 비번확인
-  if (
-    isMatch(passwordInputElement.value, passwordValidInputElement.value) ===
-      false ||
-    passwordInputElement.value.length === 0
-  ) {
-    passwordValidSuccessElement.classList.add("hide");
-    passwordValidFailElement.classList.remove("hide");
-    passwordValidCheck = 0;
-  } else {
-    passwordValidSuccessElement.classList.remove("hide");
-    passwordValidFailElement.classList.add("hide");
-    passwordValidCheck = 1;
-  }
+  let result = 0;
+  // 1. 각 요소 조건 확인하기
+  result += nameCheck(document.getElementById("userName").value.trim());
+  result += emailCheck(document.getElementById("userEmail").value.trim());
+  result += ageCheck(document.getElementById("userAge").value.trim());
+  result += passwordCheck(document.getElementById("userPassword").value.trim());
+  result += passwordValidCheck(document.getElementById("userPassword").value.trim(), document.getElementById("userPasswordValid").value.trim());
 
   // 모두 성공시 모달창 생성
-  if (
-    nameCheck + emailCheck + ageCheck + passwordCheck + passwordValidCheck ===
-    5
-  ) {
+  if (result === 5) {
     console.log("hello");
     let modalCloseButton = document.querySelector("#modalContent .close");
     let modalContainerElement = document.getElementById("modalContainer");
@@ -203,7 +148,5 @@ membershipForm.addEventListener("submit", function (event) {
       // contentElement.classList.remove("disappear");
       membershipForm.classList.remove("disappear");
     });
-
-    
   }
 });
