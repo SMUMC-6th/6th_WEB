6d8add69570e3e1d2af9e38ab377b5ebcdf6c5a5
chanmin Lee <dlcksals87@gmail.com>
2024-03-26 20:33:46 +0900

    💄미션 2 CHORE : 1주차 피드백 수용 ( overflow ... 처리, 버튼 시각 효과, 스크롤 바, input focus)

--- 노먼_이찬민/1주차 미션/미션2/index.js
+++ 노먼_이찬민/1주차 미션/미션2/index.js
@@ -1,3 +1,13 @@
+// 피드백 1 : ...으로 보이게 처리 : white-space : no-wrap, overflow : hidden, text-overflow: ellipsis
+// 피드백 2 : 버튼 효과 주기 -> transform(0.9) 
+// 피드백 3 : 항목 추가하면 스크롤 생기게 , 가장 마지막 항목을 먼저 보일수 있도록 위치 조정 (.scrollIntoView({block : center}))
+// 이 때 스크롤 창 범위를 지정해놔야 무한히 커지지 않음. 
+// 피드백 4 : input autofocus 속성을 이용해서 자동으로 빈 인풋일때 포커스되도록 
+// 피드백 5 : classList.add 말고 .setAttribute('추가할 형식타입', '추가할 형식의 name' );
+// 피드백 6 : 노드 복사할 일이 있을 때, 변수에 저장해서 옮기지 말고, .cloneNode(true(깊은복사)/False(얕은복사))
+// 깊은 복사 : 하위 요소까지 모두 복사, 얕은 복사 : 지금 요소만 복사
+
+
 let todoForm = document.getElementById("todoForm");
 let todoInput = document.getElementById("todoInput");
 let todoList = document.getElementById("todoList");
@@ -8,6 +18,7 @@ todoForm.addEventListener("submit", function (event) {
 
   createTodoListItem(todoInput.value);
   todoInput.value = "";
+  
 }); 
 
 // remove 버튼 이벤트 콜백함수
@@ -37,9 +48,13 @@ function completeCallBack(event) {
 
   // 새로운 삭제 버튼 추가
   todoItem.appendChild(removeButton);
+
+  todoInput.focus();
+  todoItem.scrollIntoView({ block: "end" });
 }
 
 function createTodoListItem(str) {
+  
   const li = document.createElement("li");
   const span = document.createElement("span");
   const button = document.createElement("button");
@@ -54,6 +69,8 @@ function createTodoListItem(str) {
   button.addEventListener("click", completeCallBack);
   todoList.appendChild(li);
   console.log(li);
+
+  li.scrollIntoView({ block: "end" });
 }
 
 
