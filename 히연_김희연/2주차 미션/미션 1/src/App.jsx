88c0f1c3355884d7ad07ae4c06133812a931cc7a
joyfulond <kimhy030927@gmail.com>
2024-03-31 00:14:55 +0900

    feat: 수정전

--- 히연_김희연/2주차 미션/미션 1/src/App.jsx
+++ 히연_김희연/2주차 미션/미션 1/src/App.jsx
@@ -1,11 +1,25 @@
 import React from "react";
 import "./App.css";
+import { movies } from "../public/data/movies";
+import Movie from "./components/Movie";
 
 const App = () => {
   return (
-    <>
-      <div className="huiyeon">미션</div>
-    </>
+    <div>
+      <div className="app-container">
+        {movies.results.map((item, idx) => {
+          return (
+            <Movie
+              key={idx}
+              title={item.title}
+              poster_path={item.poster_path}
+              vote_average={item.vote_average}
+              overview={item.overview}
+            />
+          );
+        })}
+      </div>
+    </div>
   );
 };
 
---
+++ 히연_김희연/2주차 미션/미션 1/src/components/Movie.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+
+const IMG_BASE_URL = "https://image.tmdb.org/t/p/original";
+
+export default function Movie({ title, poster_path, vote_average, overview }) {
+  const [isHover, setIsHover] = useState(false);
+
+  return (
+    <div
+      className="movie-container"
+      onMouseEnter={() => setIsHover(true)}
+      onMouseLeave={() => setIsHover(false)}
+    >
+      <img src={IMG_BASE_URL + poster_path} alt="영화포스터" />
+      <div className="movie-info">
+        <h4>{title}</h4>
+        <span>{vote_average}</span>
+      </div>
+      {isHover && (
+        <div className="movie-wrapper">
+          <h4 className="movie-explain">{title}</h4>
+          <br />
+          <div className="movie-explain">{overview}</div>
+        </div>
+      )}
+    </div>
+  );
+}
