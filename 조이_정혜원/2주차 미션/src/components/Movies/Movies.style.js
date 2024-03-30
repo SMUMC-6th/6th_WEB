9f3e76f40b65e1affeb4c62865c07e2364c38d0c
hijjoy <hijjoy@naver.com>
2024-03-30 20:01:35 +0900

    CHORE: 미디어쿼리 픽셀 변경

--- 조이_정혜원/2주차 미션/src/components/Movies/Movies.style.js
+++ 조이_정혜원/2주차 미션/src/components/Movies/Movies.style.js
@@ -6,11 +6,15 @@ export const Container = styled.div`
 
   margin: 20px 0 20px;
 
-  @media screen and (max-width: 1024px) {
+  @media screen and (min-width: 768px) and (max-width: 1023px) {
     grid-template-columns: repeat(5, 1fr);
   }
 
-  @media screen and (max-width: 768px) {
-    grid-template-columns: repeat(4, 1fr);
+  @media screen and (min-width: 480px) and (max-width: 767px) {
+    grid-template-columns: repeat(3, 1fr);
+  }
+
+  @media screen and (max-width: 479px) {
+    grid-template-columns: repeat(2, 1fr);
   }
 `;
