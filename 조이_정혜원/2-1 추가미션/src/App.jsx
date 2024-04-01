74420f73d8cd3164dd6c003f3731f8358a1e5344
hijjoy <hijjoy@naver.com>
2024-04-02 00:18:17 +0900

    CHORE: App.jsx 스타일 컴포넌트 분리

--- 조이_정혜원/2-1 추가미션/src/App.jsx
+++ 조이_정혜원/2-1 추가미션/src/App.jsx
@@ -1,27 +1,20 @@
 import Navbar from "./components/Navbar/Navbar";
 import Content from "./components/Content/Content";
 import Footer from "./components/Footer/Footer";
-import styled from "styled-components";
+
 import { useState } from "react";
+import * as S from "./App.style";
 
 function App() {
   const [click, setClick] = useState(false);
 
   return (
-    <Container>
+    <S.Container>
       <Navbar click={click} setClick={setClick} />
       <Content click={click} />
       <Footer />
-    </Container>
+    </S.Container>
   );
 }
 
 export default App;
-
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-
-  width: 100vw;
-  height: 100vh;
-`;
