8b07d9759e59d77b234fa9d713227eafb0caf2f7
hyeyoon23 <51lhy@naver.com>
2024-04-02 12:29:14 +0900

    CHORE: css 수정

--- 유니_이혜윤/2주차 미션/src/MovieList.jsx
+++ 유니_이혜윤/2주차 미션/src/MovieList.jsx
@@ -12,19 +12,19 @@ function MovieList({movie}) {
       onMouseEnter={() => setIsHovered(true)} // 마우스가 올라갔을 때
       onMouseLeave={() => setIsHovered(false)} // 마우스가 내려갔을 때
     >
-      {isHovered ? (
+
+    <img src={ `https://image.tmdb.org/t/p/original/${movie.backdrop_path}` } alt={movie.title}></img>
+    <div className="content">
+      <h2>{movie.title}</h2>
+      <p className="avg">{movie.vote_average}</p>
+    </div>
+
+      {isHovered && (
         // 마우스를 올렸을 때 보여줄 내용
         <div className="movie-overview">
-          <h2>{movie.title}</h2>
-          <p>{movie.overview}</p>
-        </div>
-      ) : (
-        // 마우스를 올리지 않았을 때 기본으로 보여줄 내용
-        <div>
-          <img src={ `https://image.tmdb.org/t/p/original/${movie.backdrop_path}` } alt={movie.title}></img>
-          <div className="content">
-            <h2>{movie.title}</h2>
-            <p className="avg">{movie.vote_average}</p>
+          <div className="movie-box">
+            <h2 className="movie-title">{movie.title}</h2>
+            <p className="movie-explain">{movie.overview}</p>
           </div>
         </div>
       )}
