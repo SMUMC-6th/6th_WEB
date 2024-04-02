f99254265025f49ff428984a114282b40a2002da
chanmin Lee <dlcksals87@gmail.com>
2024-04-02 15:10:17 +0900

    💄 2주차 추가 미션 1 CHORE : footer 색상 변경

--- 노먼_이찬민/2주차 미션/추가미션1/src/components/Footer/Footer.jsx
+++ 노먼_이찬민/2주차 미션/추가미션1/src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import * as S from './Footer.style'
 
 const Footer = ()=>{
   return (
-    <S.Footer color='black'>BANNER</S.Footer>
+    <S.Footer color='rgb(120, 170, 10)'>BANNER</S.Footer>
   )
 }
 
