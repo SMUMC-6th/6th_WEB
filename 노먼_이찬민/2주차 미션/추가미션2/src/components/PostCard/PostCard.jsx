770479418109235a1d1a6ca11efded279efe8bf6
chanmin Lee <dlcksals87@gmail.com>
2024-04-07 22:46:07 +0900

    🎨 2주차 추가 미션 2 FEAT : 클릭 시 아이콘 변경 구현

--- 노먼_이찬민/2주차 미션/추가미션2/src/components/PostCard/PostCard.jsx
+++ 노먼_이찬민/2주차 미션/추가미션2/src/components/PostCard/PostCard.jsx
@@ -3,13 +3,16 @@ import Icons from '../Icons/Icons';
 import { useState } from 'react';
 
 const PostCard = () => {
+  const ImageURL = 'https://images.unsplash.com/photo-1708771754562-163e2994c815?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDQ1fHhqUFI0aGxrQkdBfHxlbnwwfHx8fHw%3D';
+
   const [count, setCount] = useState(0);
   const [isPressed, setIsPressed] = useState(false);
   // handle이라는 관례가 있나봐...
   const handleClick = () => {
     // 하트 toggle적인 속성으로 빨갛게 -> Icons styled component에서 해결, 
     // count + 1 -> 어차피 자식 컴포넌트에서 실행되어도 이 컴포넌트의 count 상태값이 변함
-    setIsPressed(!isPressed);
+    isPressed === true? setIsPressed(false) : setIsPressed(true);
+    console.log(isPressed);
     count === 0? setCount(1) : setCount(0);
   }
 
@@ -18,7 +21,7 @@ const PostCard = () => {
       <S.CountMessage>좋아요 개수 : {count}</S.CountMessage>
       <S.CardContainer>
         <S.CardHeader>SMUMC POST</S.CardHeader>
-        <S.CardImage></S.CardImage>
+        <S.ImageContainer><S.CardImage src={ImageURL}></S.CardImage></S.ImageContainer>
         <Icons func={handleClick} isPressed={isPressed}/>
       </S.CardContainer>
     </div>
