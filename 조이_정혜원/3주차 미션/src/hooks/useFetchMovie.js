889427ade9129cdeb37414c40ecbcb345622924e
hijjoy <hijjoy@naver.com>
2024-04-09 11:18:00 +0900

    CHORE: AbortContoller 추가

--- 조이_정혜원/3주차 미션/src/hooks/useFetchMovie.js
+++ 조이_정혜원/3주차 미션/src/hooks/useFetchMovie.js
@@ -6,15 +6,22 @@ const useFetchMovie = (requestURL) => {
   const [loading, setLoading] = useState(false); // 보통 처음에는 false 데이터 받을 때 true로 변경
   const [error, setError] = useState(false);
 
+  const controller = new AbortController();
+
   const fetchMovieData = async () => {
     setLoading(true);
     setError(false);
 
     try {
-      const res = await movieAxios(requestURL);
+      const res = await movieAxios(requestURL, { signal: controller.signal });
       setMovies(res.data.results);
-    } catch (e) {
-      console.log(e);
+    } catch (err) {
+      if (err.name === "AbortError") {
+        alert("AbortError");
+      } else {
+        console.log(e);
+      }
+
       setError(true);
     } finally {
       setLoading(false);
@@ -23,6 +30,10 @@ const useFetchMovie = (requestURL) => {
 
   useEffect(() => {
     fetchMovieData();
+
+    return () => {
+      controller.abort();
+    };
   }, [requestURL]);
 
   return { movies, loading, error };
