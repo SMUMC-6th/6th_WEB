4ef0d11155e7fec9c50eb1685f10c12dc8dc487f
hyeyoon23 <51lhy@naver.com>
2024-05-01 12:52:41 +0900

    CHORE: css 수정

--- 유니_이혜윤/3주차 추가미션/src/trends_styled.js
+++ 유니_이혜윤/3주차 추가미션/src/trends_styled.js
@@ -2,38 +2,62 @@ import styled from "styled-components";
 
 const Container = styled.div `
   width: 100%;
+  height: 100vh;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
 
-  margin: 20px;
+  margin: 30px;
+`;
+
+const Wrapper = styled.div`
+  font-size: 14px;
+  h1 {
+    margin-bottom: 30px;
+  }
 `;
 
 
 const Box = styled.div`
-  border: 2px solid;
-  border-radius: 10px;
+  display: flex;
+
+  width: 400px;
+  height: 40px;
   padding: 10px;
   margin: 7px;
 
-  width: 70%;
-`;
+  border: 2px solid;
+  border-radius: 10px;
 
-const Id = styled.div`
-  width: 20%;
 
+  p {
+    font-weight: bold;
+    margin: 3px 0 0 5px;
+  }
+
+  div {
+    margin-top: 2px;
+  }
 `;
 
 const Title = styled.div`
-  width: 80%;
-  height: 50%;
-
+  font-weight: bold;
 `;
 
 const Content = styled.div`
-  width: 80%;
-  height: 50%;
+  color: gray;
+  padding-left: 10px;
 `;
 
-export {Container, Box, Title, Id, Content}
+const ButtonWrapper = styled.div`
+  margin: 20px;
+
+  button {
+    margin: 4px;
+    border: none;
+    border-radius: 6px;
+  }
+`;
+
+export {Container, Wrapper, Box, Title, Content, ButtonWrapper}
