abeab152b34daddefff78abda718e35d24224a75
joyfulond <kimhy030927@gmail.com>
2024-05-13 02:11:57 +0900

    4주차 pages 추가

--- 히연_김희연/4주차 미션/실습/src/App.jsx
+++ 히연_김희연/4주차 미션/실습/src/App.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import AppLayout from "./layout/AppLayout";
 import MainPage from "./pages/Main/MainPage";
@@ -8,14 +7,11 @@ import PopularPage from "./pages/Popular/PopularPage";
 import TopRatedPage from "./pages/TopRated/TopRatedPage";
 import NowPlayingPage from "./pages/NowPlaying/NowPlayingPage";
 import UpComingPage from "./pages/UpComing/UpComingPage";
-import MovieDetailPage from "./pages/MovieDetail/MovieDetailPage";
-import NotFound from "./pages/NotFound/NotFound";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <AppLayout />,
-    errorElement: <NotFound />,
     children: [
       {
         index: true,
@@ -36,10 +32,6 @@ const router = createBrowserRouter([
       {
         path: "/upcoming",
         element: <UpComingPage />,
-      },
-      {
-        path: "/movie/:movieName",
-        element: <MovieDetailPage />,
       },
     ],
   },
---
+++ 히연_김희연/4주차 미션/실습/src/components/Footer/Footer.style.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  background-color: #090b13;
+  width: 100%;
+  height: 20px;
+  color: white;
+  background-color: #22254a;
+  display: flex;
+  justify-content: center;
+  justify-content: flex-end;
+  align-items: center;
+  height: 45%;
+`;
+
+export { Container };
