911c30ee4dfbf49aa48941718a6d344af8875801
hijjoy <hijjoy@naver.com>
2024-05-22 14:54:21 +0900

    chore: 스캘레톤 애니메이션 추가

--- 조이_정혜원/7주차 미션/src/components/Skeleton/Skeleton.style.js
+++ 조이_정혜원/7주차 미션/src/components/Skeleton/Skeleton.style.js
@@ -1,11 +1,24 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import theme from "../../styles/theme";
+
+const loading = keyframes`
+  0% {
+    background-position: -200% 0;
+  }
+  100% {
+    background-position: 200% 0;
+  }
+`;
 
 const Img = styled.div`
   width: 170px;
   height: 250px;
   background-color: rgb(249, 249, 249, 0.1);
   margin-bottom: 10px;
+
+  background: linear-gradient(90deg, rgb(249, 249, 249, 0.1), rgb(249, 249, 249, 0.17), rgb(249, 249, 249, 0.1));
+  background-size: 200% auto;
+  animation: ${loading} 4s infinite;
 `;
 const Box = styled.div`
   ${theme.ALIGN.ROW_SPACE_AROUND};
@@ -17,6 +30,10 @@ const Box = styled.div`
     height: 20px;
     background-color: rgb(249, 249, 249, 0.1);
     border-radius: 3px;
+
+    background: linear-gradient(90deg, rgb(249, 249, 249, 0.1), rgb(249, 249, 249, 0.17), rgb(249, 249, 249, 0.1));
+    background-size: 200% auto;
+    animation: ${loading} 4s infinite;
   }
 
   p {
@@ -24,6 +41,10 @@ const Box = styled.div`
     height: 20px;
     background-color: rgb(249, 249, 249, 0.1);
     border-radius: 3px;
+
+    background: linear-gradient(90deg, rgb(249, 249, 249, 0.1), rgb(249, 249, 249, 0.17), rgb(249, 249, 249, 0.1));
+    background-size: 200% auto;
+    animation: ${loading} 4s infinite;
   }
 `;
 
